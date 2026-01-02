1790c34be1f35c970091413005785cfa1abe30e9
jan2309jr <jan2309jr@gmail.com>
2026-01-02 18:28:57 +0530

    del

--- server/db.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres-js";
-import * as schema from "@shared/schema";
-
-const client = postgres(process.env.DATABASE_URL || "postgresql://localhost:5432/suss-sis");
-export const db = drizzle(client, { schema });
